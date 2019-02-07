13442d9ae0f19004d81fbf7e675c89bdf5381249
Thomas Roberts <thom.and.roberts@gmail.com>
2019-02-07 18:53:46 +0000

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var qs = new QuartzScheduler(() => { Console.WriteLine("---"); }, 18, 52);
+            qs.Schedule();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp1/QuartzScheduler.cs
@@ -0,0 +1,66 @@
+﻿using Quartz;
+using Quartz.Impl;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class QuartzScheduler : IQuartzScheduler
+    {
+        private Action act;
+        private int hour;
+        private int min;
+
+        public QuartzScheduler(Action func,int hour,int min)
+        {
+            this.act = func;
+            this.hour = hour;
+            this.min = min;
+        }
+
+        public async void Setup()
+        {
+            // construct a scheduler factory
+            NameValueCollection props = new NameValueCollection
+                {
+                    { "quartz.serializer.type", "binary" }
+                };
+            StdSchedulerFactory factory = new StdSchedulerFactory(props);
+
+            // get a scheduler
+            IScheduler sched = await factory.GetScheduler();
+            await sched.Start();
+
+            // define the job and tie it to our HelloJob class
+            IJobDetail job = JobBuilder.Create<Job>()
+                .WithIdentity("myJob", "group1")
+                .Build();
+            job.JobDataMap.Add("action",act);
+            // Trigger the job to run now, and then every 40 seconds
+            ITrigger trigger = TriggerBuilder.Create()
+                .WithIdentity("myTrigger", "group1")
+                .StartNow()
+                .WithSchedule(CronScheduleBuilder.DailyAtHourAndMinute(hour, min))
+            .Build();
+
+            await sched.ScheduleJob(job, trigger);
+        }
+
+        public void Schedule()
+        {
+            Setup();
+        }
+    }
+
+    class Job : IJob
+    {
+        public async Task Execute(IJobExecutionContext context)
+        {
+            Action act = (Action)context.JobDetail.JobDataMap.Get("action");
+            act.Invoke();
+        }
+    }
+}
